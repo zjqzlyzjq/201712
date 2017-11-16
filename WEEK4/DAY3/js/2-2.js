89f363411d77e664b7683d8063a6afa183aad9dc
zhouxiaotian <1282347298@qq.com>
2017-11-16 12:09:57 +0800

    20171116

--- WEEK4/DAY3/js/2-2.js
+++ WEEK4/DAY3/js/2-2.js
@@ -1,13 +1,18 @@
 var oBox = document.getElementById('box');
-animate(oBox, {top: 0}, 500, function () {
-    utils.css(oBox, 'backgroundColor', 'green');
-    animate(oBox, {left: 800}, 500, function () {
-        utils.css(oBox, 'backgroundColor', 'orange');
-        animate(oBox, {top: 300}, 500, function () {
-            utils.css(oBox, 'backgroundColor', 'pink');
-            animate(oBox, {left: 400}, 500, function () {
-                utils.css(oBox, 'backgroundColor', 'red');
+
+function move() {
+    animate(oBox, {top: 0}, 500, function () {
+        utils.css(oBox, 'backgroundColor', 'green');
+        animate(oBox, {left: 800}, 500, function () {
+            utils.css(oBox, 'backgroundColor', 'orange');
+            animate(oBox, {top: 300}, 500, function () {
+                utils.css(oBox, 'backgroundColor', 'pink');
+                animate(oBox, {left: 400}, 500, function () {
+                    utils.css(oBox, 'backgroundColor', 'red');
+                });
             });
         });
     });
-});
+}
+// move();
+// setInterval(move, 2000);
